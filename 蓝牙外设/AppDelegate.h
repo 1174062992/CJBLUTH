aec2511fc8e9bba0b618cfe7c89a7ffe207976ff
1174062992 <1174062992>
2018-03-06 09:33:13 +0800

    Initial Commit

---
+++ 蓝牙外设/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  蓝牙外设
+//
+//  Created by 曹记 on 2018/3/6.
+//  Copyright © 2018年 曹记. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
